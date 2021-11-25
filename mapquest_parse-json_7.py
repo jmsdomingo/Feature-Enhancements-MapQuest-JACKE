75ebaf95c96f28d78e6db6aea22940da5411b637
NetAcad DEVASC <changeme@example.com>
2021-11-25 08:41:16 +0000

    init

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -1,6 +1,7 @@
 import urllib.parse
 import requests
 import time
+import os
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "Fog7lroZGntKj1Es8s4P62KF9xFa9LtK"
@@ -16,12 +17,23 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-while True:
+clear = lambda: os.system('clear')
+
+restart = True
+while restart == True:
     orig = input("Starting Location: ")
     if orig == "quit" or orig == "q":
+        clear()
+        print("Exiting program...")
+        time.sleep(2)
+        clear()
         break
     dest = input("Destination: ")
     if dest == "quit" or dest == "q":
+        clear()
+        print("Exiting program...")
+        time.sleep(2)
+        clear()
         break
     url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
     print("URL: " + (url))
@@ -29,29 +41,32 @@ while True:
     json_status = json_data ["info"]["statuscode"]
     if json_status == 0:
         print ("API Status: " + str(json_status) + " = A successful route call. \n")
-        print("=============================================")
+        clear()
         from prettytable import PrettyTable
         t = PrettyTable(['Starting Point', 'End Point', 'Duration', 'Kilometers', 'Fuel Used (Ltr)'])
         t.add_row([orig,dest, (json_data["route"]["formattedTime"]), str("{:.2f}".format((json_data["route"]["distance"] )*1.61)), str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78))])
         print(t)
-        print("=============================================")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             print(bcolors.WARNING + (each["narrative"]) + bcolors.ENDC + bcolors.OKGREEN + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)" + bcolors.ENDC))
-            time.sleep(3)
-        print("=============================================\n")
+            time.sleep(1)
+        
+        repeat = input("\nRestart? [Y/N]: ")
+        if repeat == "N" or repeat == "n":
+            restart = False
+            clear()
+            print("Exiting program...")
+            time.sleep(2)
+            clear()
+            break
+        else:
+            clear()
     
     elif json_status == 402:
-        print("**********************************************")
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
-        print("**********************************************\n")
+        print(bcolors.FAIL + "\nStatus Code: " + str(json_status) + "; Invalid user inputs for one or both locations.\n" + bcolors.ENDC)
 
     elif json_status == 611:
-        print("**********************************************")
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
-        print("**********************************************\n")
+        print(bcolors.FAIL + "\nStatus Code: " + str(json_status) + "; Missing an entry for one or both locations.\n" + bcolors.ENDC)
 
     else:
-        print("************************************************************************")
-        print("For Staus Code: " + str(json_status) + "; Refer to:")
+        print(bcolors.FAIL + "\nFor Staus Code: " + str(json_status) + "; Refer to:\n" + bcolors.ENDC)
         print("https://developer.mapquest.com/documentation/directions-api/status-codes")
-        print("************************************************************************\n")
